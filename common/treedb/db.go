c395458d6b724a3ca7c3e9e2be2cb517b601a6b4
jaeseong <b532882@reappay.co.kr>
2020-11-05 17:55:45 +0900

    tree형태의 DB

--- common/treedb/db.go
+++ common/treedb/db.go
@@ -1,8 +1,11 @@
 package treedb
 
-import "strings"
+import (
+	"strings"
+)
 
 type node struct {
+	depth	uint
 	tag 	string
 	child   *node
 	brother *node
@@ -10,23 +13,25 @@ type node struct {
 }
 
 func NewNode() *node {
-	return &node{"",nil, nil, nil}
+	return &node{0,"",nil, nil, nil}
 }
 
-func (n *node) AddChildNode(pNode *node) {
+func (n *node) addChildNode(pNode *node) {
 	n.child = pNode
+	n.child.depth = n.depth+1
 }
 
-func (n *node) AddBrotherNode(pNode *node) {
+func (n *node) addBrotherNode(pNode *node) {
 	n.brother = pNode
+	n.brother.depth = n.depth
 }
 
-func (n *node) LinkDataTable(pData *interface{}) {
+func (n *node) linkDataTable(pData *interface{}) {
 	n.data = pData
 }
 
 //재귀함수로 구현해서, node를 정리하도록 한다.
-//todo : 만약에 이 treedb의 깊이가 너무 깊으면 재귀함수 스택이 너무 올라간다.
+//todo : 만약에 이 treedb의 깊이가 너무 깊으면 재귀함수 스택수가 증가한다.
 func (n *node) DestoryNode() {
 	if n.child != nil {
 		n.child.DestoryNode()
@@ -38,16 +43,23 @@ func (n *node) DestoryNode() {
 	n.brother = nil
 	n.data = nil
 }
-// /root/child/child/brother ... 형식의 문자열
+
+// /depth1/depth2/depth3/depth4
+// /site/domain/server/cpu/... 형식의 문자열
 func (root *node)GetDataTable(path string) (*interface{}, error) {
 	words := strings.Split(path, "/")
 	for i := range words {
-		root.EqualTag(words[i])
+		if root.EqualTag(words[i]) {
+
+		} else {
+
+		}
 	}
+	return nil, nil
 }
 
-func (n *node)find(key string, depth int) bool {
-	return true
+func (n *node)NextNode(depth uint,tag string) (*node, error) {
+	return nil, nil
 }
 
 func (n *node)EqualTag(value string) bool {

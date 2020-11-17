1098a152feeffc3c7f9d54ed26a64b3ce40e2d39
jaeseong <b532882@reappay.co.kr>
2020-11-17 20:28:59 +0900

    treedb 수정해야

--- common/treedb/db.go
+++ common/treedb/db.go
@@ -25,29 +25,47 @@ func (n *node) SetTag(tag string) error {
 	return nil
 }
 
-// -1, add 못함
-// 0, child add
-// 1, sibling add
-func (n *node) AddNode(pNode *node) int {
-	if pNode == nil {
-		return -1
+// -2, failed to add
+// -1, existing Node
+// 0, add
+func (n *node) Add(pNode *node) int {
+	if pNode == nil || pNode.tag == "" {
+		return -2
 	}
-
 	if n.tag == pNode.tag {
 		return -1
 	}
+	if n.addChild(pNode) != nil {
+		return -1
+	} else {
+		if n.addSibling(pNode) != nil {
+			return -1
+		}
+	}
+	return 0
+}
 
+func (n *node) addChild(pNode *node) error {
 	if n.child == nil {
 		n.child = pNode
-		return 0
-	} else {
-		temp := &n.sibling
-		for *temp != nil {
-			temp = &(*temp).sibling
+		return nil
+	}
+	if n.child.tag == pNode.tag {
+		return n.addSibling(pNode)
+	}
+	return nil
+}
+
+func (n *node) addSibling(pNode *node) error {
+	temp := &n.sibling
+	for *temp != nil {
+		temp = &(*temp).sibling
+		if (*temp).tag == pNode.tag {
+			return errors.New("sibling is duplicated")
 		}
 		*temp = pNode
 	}
-	return 1
+	return nil
 }
 
 func (n *node) LinkDataTable(pData *interface{}) {
@@ -56,12 +74,12 @@ func (n *node) LinkDataTable(pData *interface{}) {
 
 //재귀함수로 구현해서, node를 정리하도록 한다.
 //todo : 만약에 이 treedb의 깊이가 너무 깊으면 재귀함수 스택수가 증가한다.
-func (n *node) DestoryNode() {
+func (n *node) DestroysNode() {
 	if n.child != nil {
-		n.child.DestoryNode()
+		n.child.DestroysNode()
 	}
 	if n.sibling != nil {
-		n.sibling.DestoryNode()
+		n.sibling.DestroysNode()
 	}
 	n.child = nil
 	n.sibling = nil
@@ -93,12 +111,10 @@ func (root *node) GenerateNodes(path string) {
 		node := NewNode()
 		node.SetTag(word)
 
-		if ret := iter.AddNode(node); ret == 0 {
+		if ret := iter.Add(node); ret == 0 {
 			iter = iter.child
-		} else if ret == 1{
-
 		} else {
-			node.DestoryNode()
+			node.DestroysNode()
 		}
 	}
 }
@@ -117,6 +133,10 @@ func (n *node) print(leftAlign int) {
 	fmt.Print(n.tag)
 	fmt.Println("")
 
-	n.child.print(leftAlign+1)
+	if n.tag == "" {
+		n.child.print(leftAlign)
+	} else {
+		n.child.print(leftAlign+1)
+	}
 	n.sibling.print(leftAlign)
 }
